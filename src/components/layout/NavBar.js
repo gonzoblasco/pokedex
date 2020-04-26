47596bad92b234d2bb989aecfd41a75d4c79e65c
Gonzalo Blasco <gonzoblasco@outlook.com>
2020-04-25 21:32:13 -0300

    TODO: * being able to change between languages * change between the many content pages

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
 
 export default class NavBar extends Component {
+  state = {
+    language: "en",
+  };
+
+  setLanguage(language) {
+    this.setState({
+      language,
+    });
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-          {
-            // eslint-disable-next-line}
-          }
-          <a className='navbar-brand col-sm-3 col-md-2 mr-0 align-items-center' disabled>Pokedex</a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+          <a className="navbar-brand mr-auto text-white" href="#">
+            Pokedex
+          </a>
+          <a
+            className="nav-item mr-4 text-white"
+            onClick={() => this.setLanguage('english')}
+          >
+            English
+          </a>
+          <a
+            className="nav-item text-white"
+            onClick={() => this.setLanguage('spanish')}
+          >
+            Español
+          </a>
         </nav>
       </div>
     );
